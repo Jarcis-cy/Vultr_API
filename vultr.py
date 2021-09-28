f856f5842df4b13ac37f0938a25732924cb489a4
Jarcis <39492464+Jarcis-cy@users.noreply.github.com>
2021-09-28 16:38:08 +0800

    Update vultr.py

--- vultr.py
+++ vultr.py
@@ -3,7 +3,7 @@
 @File   :   vultr.py
 @Time   :   2021/9/28
 @Author :   Jarcis
-@Link   :   https://github.com/Jarcis-cy
+@Link   :   https://github.com/Jarcis-cy/Vultr_API
 '''
 import requests
 import json
